ad6cd42a16c20c25a83ec038f54c231cc2dd3320
Paul Gavrikov <paulgavrikov@yahoo.de>
2020-09-28 14:28:33 +0200

    Generating sample images for documentation

--- examples/example.py
+++ examples/example.py
@@ -2,7 +2,8 @@ from tensorflow.python.keras.models import Sequential
 from tensorflow.python.keras.layers import Dense, Flatten, Conv2D, Dropout, MaxPooling2D, InputLayer, ZeroPadding2D
 import visualkeras
 from collections import defaultdict
-
+from tensorflow import keras
+from tensorflow.keras import layers
 
 # create VGG16
 image_size = 224
@@ -48,6 +49,7 @@ model.add(Conv2D(512, activation='relu', kernel_size=(3, 3)))
 model.add(ZeroPadding2D((1, 1)))
 model.add(Conv2D(512, activation='relu', kernel_size=(3, 3)))
 model.add(MaxPooling2D())
+model.add(visualkeras.FakeLayer())
 
 model.add(Flatten())
 
@@ -66,7 +68,33 @@ color_map[MaxPooling2D]['fill'] = 'red'
 color_map[Dense]['fill'] = 'green'
 color_map[Flatten]['fill'] = 'teal'
 
-visualkeras.cnn_arch(model,
-              type_ignore=[ZeroPadding2D, Dropout, Flatten, visualkeras.FakeLayer],
-              color_map=color_map,
-              dense_orientation='x', draw_volume=True, distance=10).show()
+visualkeras.cnn_arch(model, out_file='../figures/vgg16.png', type_ignore=[visualkeras.FakeLayer])
+visualkeras.cnn_arch(model, out_file='../figures/vgg16_spacing_layers.png')
+visualkeras.cnn_arch(model, out_file='../figures/vgg16_type_ignore.png',
+                     type_ignore=[ZeroPadding2D, Dropout, Flatten])
+visualkeras.cnn_arch(model, out_file='../figures/vgg16_color_map.png',
+                     color_map=color_map)
+visualkeras.cnn_arch(model, out_file='../figures/vgg16_flat.png',
+                     draw_volume=False)
+
+
+encoder_input = keras.Input(shape=(28, 28, 1), name="img")
+x = layers.Conv2D(16, 3, activation="relu")(encoder_input)
+x = layers.Conv2D(32, 3, activation="relu")(x)
+x = layers.MaxPooling2D(3)(x)
+x = layers.Conv2D(32, 3, activation="relu")(x)
+x = layers.Conv2D(16, 3, activation="relu")(x)
+encoder_output = layers.GlobalMaxPooling2D()(x)
+encoder = keras.Model(encoder_input, encoder_output, name="encoder")
+
+visualkeras.cnn_arch(encoder, out_file='../figures/encoder.png')
+
+x = layers.Reshape((4, 4, 1))(encoder_output)
+x = layers.Conv2DTranspose(16, 3, activation="relu")(x)
+x = layers.Conv2DTranspose(32, 3, activation="relu")(x)
+x = layers.UpSampling2D(3)(x)
+x = layers.Conv2DTranspose(16, 3, activation="relu")(x)
+decoder_output = layers.Conv2DTranspose(1, 3, activation="relu")(x)
+autoencoder = keras.Model(encoder_input, decoder_output, name="autoencoder")
+
+visualkeras.cnn_arch(autoencoder, out_file='../figures/autoencoder.png')
