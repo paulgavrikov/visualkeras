d586b7de30f204ee7a35c07f23a13336d1df6bfd
Paul Gavrikov <paulgavrikov@yahoo.de>
2020-09-29 10:27:56 +0200

    Adding external lib requirements to setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="visualkeras", # Replace with your own username
+    name="visualkeras",
     version="0.0.1",
     author="Paul Gavrikov",
     author_email="paul.gavrikov@hs-offenburg.de",
@@ -17,6 +17,9 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'pillow>=6.2.0'
     ],
     python_requires='>=3.6',
 )
