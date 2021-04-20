db0ad597029a1aaa2de65bed1b27f219358af264
Paul Gavrikov <paul.gavrikov@hs-offenburg.de>
2021-04-20 09:56:38 +0200

    Release 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="visualkeras",
-    version="0.0.1",
+    version="0.0.2",
     author="Paul Gavrikov",
     author_email="paul.gavrikov@hs-offenburg.de",
     description="Architecture visualization of Keras models",
